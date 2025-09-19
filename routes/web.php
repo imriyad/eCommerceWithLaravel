a4dba687380982ac55d2adcd71c0a7765aaf9151
Mahdi Kabir Riyad <114643825+imriyad@users.noreply.github.com>
2025-09-19 22:40:35 +0600

    Added run-seeder

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,13 @@ use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 
 
 // require __DIR__.'/auth.php';
+// routes/web.php
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-seeder', function () {
+    Artisan::call('db:seed', [
+        '--class' => 'RoleSeeder',
+        '--force' => true, // force in production
+    ]);
+    return 'Seeder has been run!';
+});
