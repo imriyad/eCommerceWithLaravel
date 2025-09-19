cbd1fa233964cbffd3866298c402a7816f32ec47
Mahdi Kabir Riyad <114643825+imriyad@users.noreply.github.com>
2025-09-19 22:51:02 +0600

    Added check roles

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 // require __DIR__.'/auth.php';
 // routes/web.php
 use Illuminate\Support\Facades\Artisan;
+use Spatie\Permission\Models\Role;
+
 
 Route::get('/run-seeder', function () {
     Artisan::call('db:seed', [
@@ -16,4 +18,11 @@ Route::get('/run-seeder', function () {
         '--force' => true, // force in production
     ]);
     return 'Seeder has been run!';
+});
+
+// routes/web.php
+
+Route::get('/check-roles', function () {
+    $roles = Role::all(); 
+    return $roles; // returns JSON list of all roles
 });
