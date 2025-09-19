2be022a0bfc7dbc16a4a55a2cafb8f9b67f7b479
Mahdi Kabir Riyad <114643825+imriyad@users.noreply.github.com>
2025-09-19 23:05:25 +0600

    Updated

--- routes/web.php
+++ routes/web.php
@@ -9,20 +9,48 @@ use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 // require __DIR__.'/auth.php';
 // routes/web.php
 use Illuminate\Support\Facades\Artisan;
+
+
+// Route::get('/run-seeder', function () {
+//     Artisan::call('db:seed', [
+//         '--class' => 'RoleSeeder',
+//         '--force' => true, // force in production
+//     ]);
+//     return 'Seeder has been run!';
+// });
+
+// // routes/web.php
+
+// Route::get('/check-roles', function () {
+//     $roles = Role::all(); 
+//     return $roles; // returns JSON list of all roles
+// });
+
+use App\Models\User;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
+Route::get('/create-users', function () {
+    // Admin user
+    $admin = User::firstOrCreate(
+        ['email' => 'admin@gmail.com'], // check if exists
+        [
+            'name' => 'Admin User',
+            'password' => Hash::make('87654321')
+        ]
+    );
+    $admin->assignRole('admin');
 
-Route::get('/run-seeder', function () {
-    Artisan::call('db:seed', [
-        '--class' => 'RoleSeeder',
-        '--force' => true, // force in production
-    ]);
-    return 'Seeder has been run!';
+    // Seller user
+    $seller = User::firstOrCreate(
+        ['email' => 'seller@gmail.com'], // check if exists
+        [
+            'name' => 'Seller User',
+            'password' => Hash::make('87654321')
+        ]
+    );
+    $seller->assignRole('seller');
+
+    return "Admin and Seller created successfully!";
 });
 
-// routes/web.php
-
-Route::get('/check-roles', function () {
-    $roles = Role::all(); 
-    return $roles; // returns JSON list of all roles
-});
